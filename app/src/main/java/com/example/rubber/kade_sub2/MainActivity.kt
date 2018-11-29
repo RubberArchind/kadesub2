7044b465d9d6c4c5513914d2e5b89d3057278b88
sean.makuto@gmail.com <sean.makuto@gmail.com>
2018-11-29 22:02:29 +0700

    Bottom Navigation

---
+++ app/src/main/java/com/example/rubber/kade_sub2/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.rubber.kade_sub2
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+
+            R.id.LastMatch -> {
+                message.setText(R.string.title_lm)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.NextMatch -> {
+                message.setText(R.string.title_nm)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
